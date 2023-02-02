f6e51e5c55718455ce65013d4446ec5ce4fd5ec1
Aurélien Mariaule <mariaule.aurelien@gmail.com>
2023-02-02 11:29:40 +0100

    api

--- work/index.php
+++ work/index.php
@@ -10,7 +10,7 @@ $router = new \Bramus\Router\Router();
 
 $router->get('/invoices/{number}', function ($number) {
         $pdo = new PDO('mysql:host=localhost;dbname=cogip;charset=utf8', 'root', '');
-        $resultat=$pdo->prepare("SELECT * FROM invoices WHERE id=".$number." ORDER BY create_dat ASC ");
+        $resultat=$pdo->prepare("SELECT * FROM invoices LIMIT ".$number." ORDER BY create_dat ASC ");
         $resultat->execute();
         $donnees=$resultat->fetchAll(PDO::FETCH_ASSOC);
         echo json_encode($donnees);
