5333aeacd7cf0f2817dbaa7ed00b37e9020d7e74
jonathan <jonathanbajoux.pro@gmail.com>
2023-02-09 11:33:12 +0100

    recherche barre

--- cogip-project/src/react/partials/Companies_array.jsx
+++ cogip-project/src/react/partials/Companies_array.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+/*import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 
@@ -43,4 +43,86 @@ const CompaniesTable = () => {
     );
 };
 
+export default CompaniesTable;*/
+
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+const CompaniesTable = () => {
+  const [companies, setCompanies] = useState([]);
+  const [sortType, setSortType] = useState({ key: '', order: '' });
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/companies/5")
+      .then(res => res.data)
+      .then(data => setCompanies(data))
+      .catch(err => console.error(err));
+  }, []);
+
+  const handleSort = key => {
+    const order = sortType.key === key && sortType.order === 'asc' ? 'desc' : 'asc';
+    setSortType({ key, order });
+  };
+
+  const sortedCompanies = companies
+  .filter(company => (
+    company.Name_company.toLowerCase().includes(searchTerm.toLowerCase()) || 
+    company.tva.toLowerCase().includes(searchTerm.toLowerCase()) || 
+    company.country.toLowerCase().includes(searchTerm.toLowerCase()) || 
+    company.Name_type.toLowerCase().includes(searchTerm.toLowerCase()) || 
+    new Date(company.create_dat).toLocaleDateString().includes(searchTerm)
+  ))
+  
+
+    .sort((a, b) => {
+      if (sortType.key === 'Name_company') {
+        return sortType.order === 'asc' ? a.Name_company.localeCompare(b.Name_company) : b.Name_company.localeCompare(a.Name_company);
+      } else if (sortType.key === 'create_dat') {
+        return sortType.order === 'asc' ? a.create_dat - b.create_dat : b.create_dat - a.create_dat;
+      }
+      return 0;
+    });
+
+  return (
+    <div className="array arrays__lastCompanies">
+      <h2>Last Companies</h2>
+      <section className="overflowArray">
+        <input type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+        <table>
+          <thead>
+            <tr>
+              <th onClick={() => handleSort('Name_company')}>Name
+              <button type="button" onClick={() => handleSort("create_dat")}>
+                  ^
+                </button></th>
+              <th>TVA</th>
+              <th>Country</th>
+              <th>Type</th>
+              <th onClick={() => handleSort('create_dat')}>Created At
+              <button type="button" onClick={() => handleSort("create_dat")}>
+                  ^
+                </button>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedCompanies.map(company => (
+              <tr key={company.id}>
+                <td>{company.Name_company}</td>
+                <td>{company.tva}</td>
+                <td>{company.country}</td>
+                <td>{company.Name_type}</td>
+                <td>{new Date(company.create_dat).toLocaleDateString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
+};
+
 export default CompaniesTable;
+
+
--- cogip-project/src/react/partials/Contacts_array.jsx
+++ cogip-project/src/react/partials/Contacts_array.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+/*import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 const ContactsTable = () => {
@@ -42,5 +42,84 @@ const ContactsTable = () => {
   );
 };
 
-export default ContactsTable;
+export default ContactsTable;*/
 
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+const ContactsTable = () => {
+  const [contacts, setContacts] = useState([]);
+  const [sortField, setSortField] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/contacts/5")
+      .then(res => res.data)
+      .then(data => setContacts(data))
+      .catch(err => console.error(err));
+  }, []);
+
+  const handleSort = (field) => {
+    if (sortField === field) {
+      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    } else {
+      setSortOrder("asc");
+      setSortField(field);
+    }
+  };
+
+  const sortedContacts = [...contacts].sort((a, b) => {
+    let result = 0;
+    if (sortField === "Name_contact") {
+      result = a.Name_contact.localeCompare(b.Name_contact);
+    } else if (sortField === "create_dat") {
+      result = a.create_dat - b.create_dat;
+    }
+
+    if (sortOrder === "desc") {
+      result = result * -1;
+    }
+
+    return result;
+  });
+
+  return (
+    <div className="array arrays__lastInvoices">
+      <h2>Last Contacts</h2>
+      <section className="overflowArray">
+        <table>
+          <thead>
+            <tr>
+              <th>Name
+                <button type="button" onClick={() => handleSort("Name_contact")}>
+                  ^
+                </button>
+              </th>
+              <th>Phone</th>
+              <th>Mail</th>
+              <th>Companies</th>
+              <th>Created At
+                <button type="button" onClick={() => handleSort("create_dat")}>
+                  ^
+                </button>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedContacts.map(contact => (
+              <tr key={contact.id}>
+                <td>{contact.Name_contact}</td>
+                <td>{contact.phone}</td>
+                <td>{contact.email}</td>
+                <td>{contact.Name_company}</td>
+                <td>{new Date(contact.create_dat).toLocaleDateString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
+};
+
+export default ContactsTable;
