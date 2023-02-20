a683bb1811daf679a1799c31401ef86333f759ac
Corentin Miserque <corentinmiserque1@gmail.com>
2023-02-21 00:12:02 +0100

    dashboard companies finish

--- cogip-project/src/react/partials/Dashboard/Dashboard_Companiespage/Dashboard_Companies_Modif.jsx
+++ cogip-project/src/react/partials/Dashboard/Dashboard_Companiespage/Dashboard_Companies_Modif.jsx
@@ -8,16 +8,23 @@ function Editcompanies() {
   const [name, setName] = useState("");
   const [country, setCountry] = useState("");
   const [tva, setTva] = useState("");
+  const [date, setDate] = useState(new Date());
   const [typeId, setTypeId] = useState("1");
   const [nameError, setNameError] = useState("");
   const [countryError, setCountryError] = useState("");
   const [tvaError, setTvaError] = useState("");
-
+  const { id } = useParams();
 
 
   const handleSubmit = (event) => {
     event.preventDefault();
     let formIsValid = true;
+    const form = event.target;
+    const formData = new FormData(form);
+    const name = formData.get("name");
+    const country = formData.get("country");
+    const create_dat = date.toISOString();
+    const tva = formData.get("tva");
     // Validation des champs de formulaire
     if (!name) {
       setNameError("Veuillez entrer un nom.");
@@ -25,6 +32,7 @@ function Editcompanies() {
     } else {
       setNameError("");
     }
+   
     if (!country) {
       setCountryError("Veuillez entrer un pays.");
       formIsValid = false;
@@ -32,30 +40,40 @@ function Editcompanies() {
       setCountryError("");
     }
     if (!tva) {
-      setTvaError("Veuillez entrer une TVA.");
+      setTvaError("Veuillez entrer un numéro de TVA.");
       formIsValid = false;
     } else {
       setTvaError("");
     }
+    if (!/^[A-Za-z]{2}[0-9]{10}$/.test(tva)) {
+      setTvaError("Le numéro de TVA doit contenir 2 lettres suivies de 10 chiffres.");
+      return;
+    }
 
     if (formIsValid) {
-      const data = {
-        Name: name,
-        Type_id: typeId,
-        Country: country,
-        TVA: tva,
+      const data = { 
+        name: name, 
+        type_id: typeId, 
+        country: country, 
+        tva: tva, 
+        create_dat: create_dat 
       };
       const confirmSubmit = window.confirm(
         "Êtes-vous sûr de vouloir modifier cette entreprise?"
       );
 
       if (confirmSubmit) {
-        axios
-          .patch(`https://quentin.hugoorickx.tech/company/${id}`, data)
-          .then(() => {
-            window.location.href = "/dashboard_Companiespage";
-          })
-          .catch((err) => console.log(err.message));
+        let url = `https://quentin.hugoorickx.tech/company/${id}`;
+        let options = {
+          method: "PATCH",
+          body: JSON.stringify(data),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        fetch(url, options).then(() => {
+          window.location.href = "/dashboard_Companiespage";
+        });
       }
     }
   };
@@ -69,7 +87,7 @@ function Editcompanies() {
       <label htmlFor="type_id"></label>
       <select id="type_id" name="type_id">
         <option value="1">Supplier</option>
-        <option value="2">Client</option>e
+        <option value="2">Client</option>
       </select>
 
       <label htmlFor="country"></label>
