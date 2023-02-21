806ecf69b8b5aba735e1ab7d24fbd712df076901
Aurélien Mariaule <mariaule.aurelien@gmail.com>
2023-02-21 19:44:43 +0100

    new work

--- work/class/companies.php
+++ work/class/companies.php
@@ -26,7 +26,7 @@ class companies extends Dbh {
     }
 
     public function get_companiesID($id){
-        $sql="SELECT companies.id, companies.name AS Name_company, country, tva, companies.create_dat , companies.update_dat, types.name AS Name_type
+        $sql="SELECT companies.id,type_id, companies.name AS Name_company, country, tva, companies.create_dat , companies.update_dat, types.name AS Name_type
         FROM companies 
         INNER JOIN types 
         ON companies.type_id = types.id  WHERE companies.id = $id ";
@@ -46,7 +46,7 @@ class companies extends Dbh {
         $create_dat = $data['create_dat'];
         $error = false;
 
-        if (!preg_match("/^[a-zA-Z0-9]+$/", $name)) {
+        if (!preg_match("/^[a-zA-Z0-9 ]+$/", $name)) {
             header('Content-Type: application/json');
             echo json_encode(['error'=> 'Le nom ne peut contenir que des lettres et des chiffres']);
             $error = true;
@@ -54,11 +54,11 @@ class companies extends Dbh {
             header('Content-Type: application/json');
             echo json_encode(['error'=> 'Le nombre ne peut contenir que des chiffres']);
             $error = true;
-        } elseif (!preg_match("/^[a-zA-Z]+$/", $country)) {
+        } elseif (!preg_match("/^[a-zA-Z ]+$/", $country)) {
             header('Content-Type: application/json');
             echo json_encode(['error'=> 'Le pays ne peut contenir que des lettres']);
             $error = true;
-        } elseif (!preg_match("/^[a-zA-Z]{2}[0-9]+$/", $tva)) {
+        } elseif (!preg_match("/^[a-zA-Z ]{2}[0-9 ]+$/", $tva)) {
             header('Content-Type: application/json');
             echo json_encode(['error'=> 'La TVA doit commencer par 2 lettres, suivies de chiffres.']);
             $error = true;
@@ -71,6 +71,18 @@ class companies extends Dbh {
           $create_dat = filter_var($create_dat, FILTER_SANITIZE_STRING);
         }        
         if(!$error){
+            // Vérification si l'entreprise existe déjà
+            $sql = "SELECT * FROM companies WHERE name = :name AND tva = :tva";
+            $stmt = $this->connect()->prepare($sql);
+            $stmt->execute([
+                ':name' => $name,
+                ':tva' => $tva
+            ]);
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($row) {
+                header('Content-Type: application/json');
+                echo json_encode(['error'=> 'Cette compagnie existe déjà dans la base de données.']);
+            } else {
         $sql="INSERT INTO companies (name, type_id, country, tva, create_dat) VALUES (:name, :type_id, :country, :tva, :create_dat)";
         $add=$this->connect()->prepare($sql);
         $add->bindValue(':name', $data["name"]);
@@ -82,6 +94,7 @@ class companies extends Dbh {
         header('Content-Type: application/json');
         echo json_encode(['message' => 'Données reçues avec succès']);
         }
+    }
     }
 
     public function patch_companie($id){
@@ -129,7 +142,9 @@ class companies extends Dbh {
         }
     }
     public function delete_companie($id){
-        $sql=" DELETE FROM companies WHERE id = $id ";
+        $sql="DELETE FROM invoices WHERE id_company = $id;
+        DELETE FROM contacts WHERE company_id =  $id;
+        DELETE FROM companies WHERE id =  $id";
         $resultat=$this->connect()->prepare($sql);
         $resultat->execute();
         $donnees=$resultat->fetchAll(PDO::FETCH_ASSOC);
